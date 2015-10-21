86c99ec3b3907f550406998a1d8bbf85edb6c54f
Noah Huber-Feely <nhuberfeely@gmail.com>
2015-10-21 00:43:40 +0000

    cleaning up code and variable names

--- extras/example/service/example-tests.py
+++ extras/example/service/example-tests.py
@@ -1,22 +1,17 @@
 import os
-import hags.api as flaskr
+import hags.api as app_handle
 import unittest
 import tempfile
 import json
 
 class FlaskrTestCase(unittest.TestCase):
     def setUp(self):
-        self.app = flaskr.app.test_client()
+        self.app = app_handle.app.test_client()
 
     def test_create_usr(self):
         credsplain = {'email_address': 'ex@ex.ex', 'password': '12345',}
         credentials = json.dumps(credsplain)
         print self.app.post('/users/', data=credentials, content_type='application/json', follow_redirects=True)
-
-'''
-    def test_me(self):
-        print self.app.get('/users/me')
-'''
 
 if __name__ == '__main__':
     unittest.main()
